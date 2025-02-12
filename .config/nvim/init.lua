eb841195d1b56ed6ce8869d1919c39bbd1c314d7
mitTheos <tim2kaspar@gmail.com>
2025-02-12 14:33:34 +0100

    add nvim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,5 @@
+-- Tab size
+vim.cmd("set expandtab ts=4 sw=4")
+
+require("config.lazy")
+vim.cmd("Neotree show")
---
+++ .config/nvim/lua/plugins/conform-spec-tst.lua
@@ -0,0 +1,33 @@
+return {
+	"stevearc/conform.nvim",
+	dependencies = { "mason.nvim" },
+	lazy = true,
+	cmd = "ConformInfo",
+	keys = {
+		{
+			"<leader>cF",
+			function()
+				require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
+			end,
+			mode = { "n", "v" },
+			desc = "Format Injected Langs",
+		},
+	},
+	opts = {
+		formatters_by_ft = {
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+			javascriptreact = { "prettier" },
+			typescriptreact = { "prettier" },
+			svelte = { "prettier" },
+			css = { "prettier" },
+			html = { "prettier" },
+			json = { "prettier" },
+			yaml = { "prettier" },
+			markdown = { "prettier" },
+			graphql = { "prettier" },
+			lua = { "stylua" },
+			python = { "isort", "black" },
+		},
+	},
+}
